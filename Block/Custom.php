a787d13330d0cd585c5f582f62d9ffb1a7d865ac
Jisse Reitsma <jisse@yireo.com>
2018-06-11 16:43:29 +0100

    Pending changes

--- Block/Custom.php
+++ Block/Custom.php
@@ -1,15 +1,35 @@
 <?php
 namespace Yireo\ExampleCmsBlock\Block;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\AbstractBlock;
+use Magento\Framework\View\Element\Context;
 
 class Custom extends AbstractBlock
 {
     const CONFIG_PATH = 'cms/yireo_examplecmsblock/block_name';
 
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        Context $context,
+        array $data = []
+    ){
+        parent::__construct($context, $data);
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @return string
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
     protected function _toHtml() : string
     {
-        $blockId = $this->_scopeConfig->getValue(self::CONFIG_PATH);
+        $blockId = $this->scopeConfig->getValue(self::CONFIG_PATH);
 
         if (empty($blockId)) {
             return __('Yireo_ExampleCmsBlock is not configured yet');
